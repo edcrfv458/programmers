ef746ced21f658436ba15d86af383a6cf65fe7a4
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-07-25 14:05:41 +0900

    [level 0] Title: 소인수분해, Time: 2.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,11 +1,12 @@
 def solution(n):
-    ad = [i for i in range(1, n+1) if n % i == 0]
     a = []
-    for i, d in enumerate(ad):
+    for i in range(1,n+1):
+        if n % i != 0:
+            continue
         count = 0
-        for j in range(1,d+1):
-            if d % j == 0:
+        for j in range(1,i+1):
+            if i % j == 0:
                 count += 1
         if count == 2:
-            a.append(d)
+            a.append(i)
     return a
