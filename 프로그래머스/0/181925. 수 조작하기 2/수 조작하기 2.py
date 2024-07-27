61971c66f4ae533c807926a7f7ced15901b1a84d
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-07-27 16:18:38 +0900

    [level 0] Title: 수 조작하기 2, Time: 17.90 ms, Memory: 14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,7 @@
+def solution(numLog):
+    answer = ''
+    c = {1:'w', -1:'s', 10:'d', -10:'a'}
+    for i in range(len(numLog)-1):
+        result = numLog[i+1] - numLog[i]
+        answer += c[result]
+    return answer
