3f40a42e5c5bb0a1ab8d82bf2b5dba61697e8139
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-07-29 16:17:44 +0900

    [level 0] Title: 분수의 덧셈, Time: 204.90 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,7 +1,11 @@
 import math
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    n, d = denom2 * numer1 + denom1 * numer2, denom1 * denom2
-    if math.gcd(n, d) != 1:
-        n, d = n // math.gcd(n, d), d // math.gcd(n, d)
-    return [n, d]
+    numer, denom = numer1 * denom2 + numer2 * denom1, denom1 * denom2
+    
+    # 분자의 약수와 분모의 약수
+    up = [i for i in range(1, numer+1) if numer % i == 0]
+    down = [i for i in range(1, denom+1) if denom % i == 0]
+    
+    # 최대 공약수
+    gcd = max([i for i in up if i in down])
+    return [numer//gcd, denom//gcd]
