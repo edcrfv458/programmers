b97735c9d83e9231491800d9db2c28892b82c5b9
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-07-31 14:55:55 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,18 +1,4 @@
 def solution(numbers):
-    numbers_map = {
-        'zero' : '0',
-        'one' : '1',
-        'two' : '2',
-        'three' : '3',
-        'four' : '4',
-        'five' : '5',
-        'six' : '6',
-        'seven' : '7',
-        'eight' : '8',
-        'nine' : '9'
-    }
-    
-    for eng, num in numbers_map.items():
-        numbers = numbers.replace(eng, num)
-        
+    for num, eng in enumerate(['zero','one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+        numbers = numbers.replace(eng, str(num))
     return int(numbers)
