5f4e65fde363a9e84fae713885e3020afad83a9e
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-12 17:03:25 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -2,13 +2,15 @@ def solution(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 1:
-                for p in range(max(0 ,i-1), min(len(board), i+2)):
-                    for q in range(max(0, j-1), min(len(board[0]), j+2)):
+                for p in range(max(0, i-1), min(i+2, len(board))):
+                    for q in range(max(0, j-1), min(j+2, len(board[0]))):
                         if board[p][q] == 0:
                             board[p][q] = 2
+                            
     count = 0
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 count += 1
+    
     return count
