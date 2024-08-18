3cab2bbd64fbda5777fafd6a9b8446a8be122c08
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-18 16:01:29 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,14 +1,20 @@
 def solution(str_list):
-    if "l" in str_list and "r" in str_list:
-        l_idx = str_list.index("l")
-        r_idx = str_list.index("r")
-        if l_idx < r_idx:
-            return str_list[:l_idx]
-        else:
-            return str_list[r_idx+1:]
-    elif "l" in str_list and "r" not in str_list:
-        return str_list[:str_list.index("l")]
-    elif "r" in str_list and "l" not in str_list:
-        return str_list[str_list.index("r")+1:]
+    # l과 r이 둘 다 있을 때
+    if 'l' in str_list and 'r' in str_list:
+        for i, s in enumerate(str_list):
+            if s == 'l':
+                return str_list[:i]
+            elif s == 'r':
+                return str_list[i+1:]
+    
+    # l만 있고 r이 없을 때
+    elif 'l' in str_list and 'r' not in str_list:
+        return str_list[:str_list.index('l')]
+    
+    # r만 있고 l이 없을 때
+    elif 'r' in str_list and 'l' not in str_list:
+        return str_list[str_list.index('r')+1:]
+    
+    # l과 r이 둘 다 없을 때
     else:
-        return []    
+        return []
