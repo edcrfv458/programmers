707be288d5b95c9463fbd93d7818902807898f8a
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-21 17:20:11 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,8 +1,2 @@
 def solution(order):
-    sum = 0
-    for c in order:
-        if 'cafelatte' in c:
-            sum += 5000
-        else:
-            sum += 4500
-    return sum
+    return sum([5000 if 'cafelatte' in c else 4500 for c in order])
