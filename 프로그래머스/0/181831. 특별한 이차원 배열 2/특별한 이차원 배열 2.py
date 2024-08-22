cee80ff641306a22b66bd0dccda7b358c6f0ec30
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-22 18:36:01 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.29 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,10 +1,6 @@
 def solution(arr):
-    l = len(arr)
-    answer = 1    
-    h = 0
-    for i in range(h, l):
-        for j in range(h, l):
-            if (arr[i][j] != arr[j][i]):
-                answer = 0
-        h += 1    
-    return answer
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
