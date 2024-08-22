907d35cdaacbb94eab9dc36e80a9573b31389c97
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-22 19:28:23 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,8 +1,5 @@
 def solution(array):
-    answer = 0
-    for a in array:
-        data = str(a)
-        for d in data:
-            if d == '7':
-                answer += 1
-    return answer
+    count = 0
+    for i in array:
+        count += str(i).count('7')
+    return count
