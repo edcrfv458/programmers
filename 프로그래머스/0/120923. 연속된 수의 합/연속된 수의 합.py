209fabaf13b237f2349047bb014a74df4515b494
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-22 19:52:07 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,20 +1,3 @@
 def solution(num, total):
-    answer = [0] * num  # 초기화된 리스트 생성
-    
-    mid = total // num  # 중간 값 계산
-    
-    if num % 2 == 1:  # 홀수인 경우
-        answer[num // 2] = mid
-        for i in range(1, num // 2 + 1):
-            answer[num // 2 - i] = mid - i
-            answer[num // 2 + i] = mid + i
-    else:  # 짝수인 경우
-        mid_left = total // num
-        mid_right = total // num + 1
-        answer[num // 2 - 1] = mid_left
-        answer[num // 2] = mid_right
-        for i in range(1, num // 2):
-            answer[num // 2 - 1 - i] = mid_left - i
-            answer[num // 2 + i] = mid_right + i
-    
-    return answer
+    x = (total - sum([i for i in range(1, num)])) // num
+    return [i for i in range(x, num+x)]
