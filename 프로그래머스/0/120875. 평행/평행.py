25687189e71a40680055507594f33d6cd93be420
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-24 11:58:10 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,24 +1,25 @@
 def solution(dots):
-    a,b = dots[0]
-    c,d = dots[1]
-    e,f = dots[2]
-    g,h = dots[3]
+    x1, y1 = dots[0]
+    x2, y2 = dots[1]
+    x3, y3 = dots[2]
+    x4, y4 = dots[3]
     
-    def inclination(x1,y1,x2,y2,x3,y3,x4,y4):
-        l_u, l_d, r_u, r_d = x2-x1, y2-y1, x4-x3, y4-y3
-        if abs(l_u * r_d) == abs(l_d * r_u):
+    def s(a1,b1,a2,b2,a3,b3,a4,b4):
+        lu, ld = a2-a1, b2-b1
+        ru, rd = a4-a3, b4-b3
+        if lu*rd == ld*ru:
             return 1
     
-    # 1: (a,b),(c,d) 기울기 & (e,f),(g,h) 기울기 비교
-    if inclination(a,b,c,d,e,f,g,h) == 1:
+    # 0,1   &   2,3
+    if s(x1,y1,x2,y2,x3,y3,x4,y4) == 1:
         return 1
     
-    # 2: (a,b),(e,f) 기울기 & (c,d),(g,h) 기울기 비교
-    if inclination(a,b,e,f,c,d,g,h) == 1:
+    # 0,2   &   1,3
+    if s(x1,y1,x3,y3,x2,y2,x4,y4) == 1:
         return 1
     
-    # 3: (a,b),(g,h) 기울기 & (e,f),(c,d) 기울기 비교
-    if inclination(a,b,g,h,c,d,e,f) == 1:
+    # 0,3   &   1,2
+    if s(x1,y1,x4,y4,x2,y2,x3,y3) == 1:
         return 1
     
     return 0
