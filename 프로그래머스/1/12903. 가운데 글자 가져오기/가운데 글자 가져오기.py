8785b506cc271b67bddee72e1c3acf059aeb1d75
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-26 13:08:26 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,6 +1,10 @@
 def solution(s):
+    # s 문자열 길이의 절반
     m = len(s) // 2
+    
+    # s의 길이가 홀수라면
     if len(s) % 2 == 1:
-        return s[m]
+        return s[m]     # 가운데 요소 1개 리턴
+    # 짝수라면
     else:
-        return s[m-1:m+1]
+        return s[m-1:m+1]   # 가운데 요소 2개 리턴
