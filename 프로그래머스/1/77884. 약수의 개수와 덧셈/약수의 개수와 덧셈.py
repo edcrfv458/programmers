e9653f8e2a82e88b05119d70e7cad910359cb66d
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-26 15:47:43 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 22.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,9 +1,9 @@
 def solution(left, right):
-    result = 0
-    for i in range(left, right+1):
-        data = [j for j in range(1, i+1) if i % j == 0]
-        if len(data) % 2 == 0:
-            result += i
-        else:
-            result -= i
-    return result
+    result = 0      # 전체 결과를 저장할 변수
+    for i in range(left, right+1):      # left ~ right 사이의 수
+        data = [j for j in range(1, i+1) if i % j == 0]     # 각 수의 약수를 계산
+        if len(data) % 2 == 0:      # 약수의 수가 짝수개이면
+            result += i     # result에 i를 더함
+        else:   # 약수의 수가 홀수개라면
+            result -= i     # result에서 i를 뺌
+    return result  
