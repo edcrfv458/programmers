e16334ea7e2452e0d229547746e8d7487a426689
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-08-26 12:48:33 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 2173.88 ms, Memory: 278 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,3 +1,8 @@
 def solution(n):
+    # i를 0에서 int(root(n)) 까지 반복하며
+    # i+1의 제곱을 result 리스트에 저장
     result = [(i+1)**2 for i in range(int(n**(1/2)) + 1)]
+    
+    # n이 result 리스트 안에 존재하면 리스트의 마지막 요소를
+    # n이 result 리스트 안에 존재하지 않으면 -1을 리턴
     return result[-1] if n in result else -1
