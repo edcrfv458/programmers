eb245168092a18af729f1be10a910762c50a35e9
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-04 17:02:19 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,11 +1,10 @@
 def solution(d, budget):
-    answer = 0  # 각 부서의 지원 금액을 더할 변수
-    result = 0  # 지원해준 부서의 수를 더할 변수
-    # d 리스트를 오름차순으로 정렬해서 접근
+    answer = 0
+    count = 0
     for i in sorted(d):
-        result += i     # 각 부서의 지원금을 더함
-        if result <= budget:    # 지원해준 전체 금액이 예산보다 적으면
-            answer += 1     # answer 1 증가
-        else:   # 많으면
-            return answer   # answer 리턴
-    return answer   # 전부다 지원해줬으면 answer 리턴
+        if answer + i <= budget:
+            answer += i
+            count += 1
+        else:
+            continue
+    return count
