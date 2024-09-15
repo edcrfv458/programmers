5182390cdc174bd74373c8ce4bae441697a116d3
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-15 16:41:44 +0900

    [level 1] Title: 기사단원의 무기, Time: 995.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,24 +1,15 @@
-import math
-
 def solution(number, limit, power):
     count = 0
-    
-    def count_divisors(n):
-        divisors = 0
-        sqrt_n = int(math.sqrt(n))
-        for i in range(1, sqrt_n + 1):
-            if n % i == 0:
-                if i == n // i:  # 제곱근인 경우
-                    divisors += 1
+    for i in range(1, number+1):
+        answer = 0
+        for j in range(1, int(i**0.5) + 1):
+            if i % j == 0:
+                if j == i // j:
+                    answer += 1
                 else:
-                    divisors += 2
-        return divisors
-    
-    for i in range(1, number + 1):
-        divisors = count_divisors(i)
-        if divisors <= limit:
-            count += divisors
+                    answer += 2
+        if answer <= limit:
+            count += answer
         else:
             count += power
-            
     return count
