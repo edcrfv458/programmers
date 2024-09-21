67a74507074dfb33e23f4bf6dd29358340bce117
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-21 14:24:12 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,13 +1,14 @@
 def solution(n):
-    answer = []     # n을 3진법으로 만들고 뒤집은 리스트를 저장
-    # n // 3이 3 이상일 동안 반복
+    data = []
     while n > 0:
-        answer.append(n % 3)   # n을 3으로 나누었을 때 몫을 저장
+        data.append(n % 3)
         n //= 3
-        
-    s = 0       # 뒤집은 3진법에 10진법으로 표현했을때 수를 저장
-    # answer 리스트를 거꾸로 반복
-    for i, d in enumerate(answer[::-1]):
-        # 3의 i제곱수를 한 결과를 s에 더함
-        s += d * (3 ** i)
-    return s
+    
+    answer = 0  # 3진법을 10진법으로 변환한 수 저장
+    upper = 0   # 지수
+    
+    for i in data[::-1]:
+        answer += i * (3**upper)
+        upper += 1
+    
+    return answer
