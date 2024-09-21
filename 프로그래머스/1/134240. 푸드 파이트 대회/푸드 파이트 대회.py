f8d75b2d6cd6b766a9ec7526f4f029f0377f82ce
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-21 15:48:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.65 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,11 @@
 def solution(food):
-    answer = []     # 선수가 먹을 음식 리스트
+    answer = []
     for i, f in enumerate(food):
-        # 음식의 개수가 2개 미만이면 패스
-        if f < 2:
+        if i == 0:
             pass
-        # 2개 이상이라면
-        else:
-            # i 요소를 f//2개 만큼 추가
-            answer += [i] * (f // 2)
-    # 정수 리스트를 문자열로 바꾸기 위해서느 ''.join(map(str, 리스트)) 사용
-    return ''.join(map(str, answer)) + '0' + ''.join(map(str, answer[::-1]))
+        for j in range(f // 2):
+            answer.append(i)
+    reverse_answer = answer[::-1]
+    answer.append(0)
+    answer.extend(reverse_answer)
+    return ''.join(map(str, answer))
