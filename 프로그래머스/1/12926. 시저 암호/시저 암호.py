9190311b38766855ec67083c494931e6f2b7cfa9
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-21 14:35:31 +0900

    [level 1] Title: 시저 암호, Time: 3.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,19 +1,18 @@
 def solution(s, n):
-    answer = ""     # 문자열을 저장할 변수
+    answer = ""
     for a in s:
         i = 0
-        if a == ' ':    # a가 공백이면 문자열에 그대로 추가
-            answer += ' '
-        else:
-            while i < n:
-                if a == 'z':    # z라면
-                    a = 'a'     # a로 만듦
-                    i += 1
-                elif a == 'Z':  # Z라면
-                    a = 'A'     # A로 만듦
-                    i += 1
-                else:
-                    a = chr(ord(a) + 1)     # z와 Z가 아니면 다음 문자
-                    i += 1
-            answer += a
+        while i < n:
+            if a == 'z':
+                a = 'a'
+                i += 1
+            elif a == 'Z':
+                a = 'A'
+                i += 1
+            elif a == ' ':
+                break
+            else:
+                a = chr(ord(a) + 1)
+                i += 1
+        answer += a
     return answer
