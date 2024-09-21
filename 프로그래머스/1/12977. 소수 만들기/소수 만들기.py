fa3f03a9b8d26b83305fdab06e8d115de17b4f15
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-09-21 16:30:20 +0900

    [level 1] Title: 소수 만들기, Time: 73.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,13 +1,13 @@
 def solution(nums):
-    count = 0   # 3개 요소 합쳐서 소수인 묶음의 수를 저장할 변수
-    # i는 0부터 len(nums)-3의 인덱스까지
+    answer = 0
     for i in range(len(nums)-2):
-        # j는 i+1부터 len(nums) - 2의 인덱스까지
         for j in range(i+1, len(nums)-1):
-            # k는 j+1부터 len(nums) - 1의 인덱스까지
             for k in range(j+1, len(nums)):
-                s = nums[i] + nums[j] + nums[k]     # 3개 요소를 합침
-                a = [t for t in range(1, s+1) if s % t == 0]    # 약수가 2개이면 소수
-                if len(a) == 2:
-                    count += 1
-    return count
+                s = nums[i] + nums[j] + nums[k]
+                c = 0
+                for t in range(2, int(s**0.5) + 1):
+                    if s % t == 0:
+                        c += 1
+                if c == 0:
+                    answer += 1
+    return answer
