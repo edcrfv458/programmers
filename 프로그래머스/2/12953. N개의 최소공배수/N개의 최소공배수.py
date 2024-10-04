e0bf2422a4081ba2092a186f9e3d711fbe06ccc1
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-04 17:20:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,9 +1,12 @@
 from functools import reduce
 
+# 최대 공약수 구하는 함수
 def GCD(x, y):
     while y:
         x, y = y, x%y
     return x
 
 def solution(arr):
+    # reduce를 이용해 두 수 간의 최소 공배수를 계산
+    # (x*y) // GCD(x,y) => 최소 공배수 구하는 식
     return reduce(lambda x, y: (x*y)//GCD(x,y), arr)
