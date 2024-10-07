072855ae0d29d777d23994f54bc423fd7314c85b
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-07 15:11:03 +0900

    [level 2] Title: 주식가격, Time: 145.90 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,10 +1,11 @@
 def solution(prices):
-    answer = [0] * len(prices)
-    for i in range(len(prices)):
-        count = 0
-        for j in range(i+1, len(prices)):
-            count += 1
+    l = len(prices)
+    answer = []
+    for i in range(l):
+        c = 0
+        for j in range(i+1, l):
+            c += 1
             if prices[i] > prices[j]:
                 break
-        answer[i] = count
+        answer.append(c)
     return answer
