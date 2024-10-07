87520a0b1c1e42c7146b706d4a0ba05b12c4cf22
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-07 17:54:01 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,6 +1,6 @@
 def solution(n):
-    root = n**0.5
-    if int(root) == float(root):
-        return (root+1)**2
+    c = n**0.5
+    if int(c) == float(c):
+        return (c+1)**2
     else:
         return -1
