c93aca68a022baaf5b464cfa3838447835f03a89
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-08 13:48:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 63.58 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,7 @@
 def solution(arr):
-    data = [arr[0]]     # 비교를 위해 첫 요소를 담음
-    for i in range(1, len(arr)):
-        if arr[i] != data[-1]:
-            data.append(arr[i])
+    data = [arr[0]]
+    arr_data = arr[1:]
+    for i in arr_data:
+        if i != data[-1]:
+            data.append(i)
     return data
