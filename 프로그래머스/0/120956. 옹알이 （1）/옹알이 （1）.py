e23bd112a41b93b82d0f11ed939d69f39b99f1b8
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-14 15:50:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,11 +1,7 @@
 def solution(babbling): # 'aya', 'ye', 'woo', 'ma'
     count = 0
     for b in babbling:
-        data = b.replace('aya', ' ')
-        data = data.replace('ye', ' ')
-        data = data.replace('woo', ' ')
-        data = data.replace('ma', ' ')
-        data = data.replace(' ','')
-        if len(data) == 0:
+        b = b.replace('aya', ' ').replace('ye', ' ').replace('woo', ' ').replace('ma', ' ').replace(' ', '')
+        if not b:
             count += 1
     return count
