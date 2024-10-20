b7ba5befb42bae0dbc97726fcb23e638a1f1764e
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-21 01:28:00 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,6 +1,6 @@
 def solution(left, right):
-    answer = 0  # 약수의 개수가 짝수면 더하고, 홀수면 뺌
-    for i in range(left, right+1):
+    answer = 0
+    for i in range(left, right + 1):
         if int(i**0.5) == float(i**0.5):
             answer -= i
         else:
