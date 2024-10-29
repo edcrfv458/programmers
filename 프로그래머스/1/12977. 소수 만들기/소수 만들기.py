3817f39f0a75cea1ec3d326de751552338d9f9e0
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-10-29 13:29:47 +0900

    [level 1] Title: 소수 만들기, Time: 81.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,13 +1,14 @@
 def solution(nums):
     answer = 0
-    for i in range(len(nums)-2):
-        for j in range(i+1, len(nums)-1):
-            for k in range(j+1, len(nums)):
+    l = len(nums)
+    for i in range(l-2):
+        for j in range(i+1, l-1):
+            for k in range(j+1, l):
                 s = nums[i] + nums[j] + nums[k]
-                c = 0
-                for t in range(2, int(s**0.5) + 1):
-                    if s % t == 0:
-                        c += 1
-                if c == 0:
+                s_sosu = []
+                for h in range(2, int(s**0.5)+1):
+                    if s % h == 0:
+                        s_sosu.append(h)
+                if not s_sosu:
                     answer += 1
     return answer
