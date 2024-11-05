89c3fb568642ad9763441bd926b2be6ec980b8b8
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-11-05 23:56:47 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -3,5 +3,5 @@ def solution(score):
     data = [s+e for s,e in score]
     sorted_data = sorted(data, reverse=True)
     for i in data:
-        result.append(sorted_data.index(i)+1)
+        result.append(sorted_data.index(i) + 1)
     return result
