c82b3f65e1525e36ba28f8c9d2c8dbd374233075
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-11-25 23:53:24 +0900

    [level 0] Title: 배열 만들기 2, Time: 718.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,7 +1,14 @@
+
 def solution(l, r):
-    data = []
+    answer = []
     for i in range(l, r+1):
-        i = str(i)
-        if not i.replace('5', '').replace('0',''):
-            data.append(int(i))
-    return data if data else [-1]
+        i_str = str(i)
+        i_set = set(i_str)
+        if len(i_set) == 1 and "0" in i_set:
+            answer.append(i)
+        elif len(i_set) == 1 and "5" in i_set:
+            answer.append(i)
+        elif len(i_set) == 2 and "0" in i_set and "5" in i_set:
+            answer.append(i)
+    return answer if answer else [-1]
+        
