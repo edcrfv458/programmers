d46eebedae9411cab61fb6d5f25dc56322cecc3d
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-12-03 21:22:18 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1562.17 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,31 +1,15 @@
 def solution(X, Y):
-    # x, y = [], []
-    # for i in X:
-    #     x.append(i)
-    # for i in Y:
-    #     y.append(i)
-    # xy_set = set(x) & set(y)
-    # if not xy_set:
-    #     return "-1"
-    # data = []
-    # for i in xy_set:
-    #     data.append(i * min(x.count(i), y.count(i)))
-    # return str(int(''.join(sorted(data, reverse=True))))
-    answer = []
-    X_data, Y_data = [], []
+    x, y = [], []
     for i in X:
-        X_data.append(i)
+        x.append(i)
     for i in Y:
-        Y_data.append(i)
-    X_set, Y_set = set(X_data), set(Y_data)
-    XY_set = X_set & Y_set
-    
-    if not XY_set:
+        y.append(i)
+    xy_set = set(x) & set(y)
+    if not xy_set:
         return "-1"
-    
     data = []
+    for i in xy_set:
+        data += i * min(x.count(i), y.count(i))
+    sorted_data = sorted(data, reverse=True)
+    return "0" if sorted_data[0] == "0" else ''.join(sorted_data) 
     
-    for i in XY_set:
-        data.append(i * min(X_data.count(i), Y_data.count(i)))
-    
-    return "0" if len(XY_set) == 1 and "0" in XY_set else ''.join(sorted(data, reverse=True))
