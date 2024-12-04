85878f037d2c0c70a77c6e3155cf0eaee5405093
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-12-04 23:46:39 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,12 +1,10 @@
 def solution(n):
-    answer = [n]
-    i = 0
-    while answer[i] != 1:
-        if answer[i] % 2 == 0:
-            next = answer[i] // 2
-        else:
-            next = 3 * answer[i] + 1
-        
-        answer.append(next)
-        i += 1
-    return answer
+    answer = []
+    while True:
+        answer.append(n)
+        if n == 1:
+            return answer
+        elif n % 2 == 0:
+            n /= 2
+        elif n % 2 == 1:
+            n = 3 * n + 1
