978570bbd1d0f93287d4eeb3f9c6b1e329ef4d9c
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2024-12-19 09:52:35 +0900

    [level 1] Title: 햄버거 만들기, Time: 205.24 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,11 +1,11 @@
 def solution(ingredient):
-    # 1: 빵, 2: 야채, 3: 고기
-    answer = []
+    # 1:빵, 2:야채, 3: 고기
+    h = []
     count = 0
     for i in ingredient:
-        answer.append(i)
-        if answer[-4:] == [1, 2, 3, 1]:
+        h.append(i)
+        if h[-4:] == [1, 2, 3, 1]:
             for _ in range(4):
-                answer.pop()
+                h.pop()
             count += 1
     return count
