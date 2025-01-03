f4e266aee957b2532fc4acd76042952df0ade414
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-03 09:46:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,13 @@
 def solution(num):
-    count = 0   # 진행된 스텝 수를 저장할 변수
+    c = 0
+    while c < 500:
+        if num == 1:
+            return c  
+        
+        if num % 2 == 0:
+            num = num // 2
+        elif num % 2 == 1:
+            num = num * 3 + 1
+        c += 1
     
-    # num이 1이 아닐 동안 반복
-    while num != 1:
-        if num % 2 == 0:    # 짝수라면
-            num //= 2       # 반으로 나누고
-            count += 1      # count 증가
-        else:               # 홀수라면
-            num = num*3 + 1 # 3배하고 1더함
-            count += 1      # count 증가
-        if count >= 500:    # count가 500이 넘으면
-            return -1       # -1 리턴
-    return count            # count 리턴
+    return -1
