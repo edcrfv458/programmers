9a34dd70a43a89a63d6714bd30145f66d7330627
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-03 09:13:47 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,9 @@
 def solution(a):
-    if a > 0 and a < 90:
+    if 0 < a < 90:
         return 1
     elif a == 90:
         return 2
-    elif a > 90 and a < 180:
-        return 3
-    else:
+    elif a == 180:
         return 4
+    else:
+        return 3
