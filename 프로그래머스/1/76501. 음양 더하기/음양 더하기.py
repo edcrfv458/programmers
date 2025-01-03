46ee3e4b23bc01e20dc89efa9c657aa17b899276
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-03 09:51:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,9 +1,8 @@
 def solution(absolutes, signs):
-    result = 0      # 결과를 저장할 변수
-    # enumerate 이용해 요소의 인덱스와 내용 이용
-    for i, s in enumerate(signs):
-        if s == True:   # s가 True이면
-            result += absolutes[i]  # result에 더함
-        else:   # s가 False이면
-            result -= absolutes[i]  # result에서 뺌
-    return result   # result 리턴
+    answer = 0
+    for i, sign in enumerate(signs):
+        if sign == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
