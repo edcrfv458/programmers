b9043d61d7bdf9019bd6ffb5871dc88ebe04656c
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-03 09:41:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,6 +1,4 @@
 def solution(x):
-    # x의 모든 자리의 수의 합
-    d = sum([int(i) for i in str(x)])
+    s = sum(int(i) for i in str(x))
     
-    # x가 d로 나누어떨어지면 True, 나누어떨어지지않으면 False 리턴
-    return True if x % d == 0 else False
+    return True if x % s == 0 else False
