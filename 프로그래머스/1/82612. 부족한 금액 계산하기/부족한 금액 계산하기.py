b029c8a1783cd7a545f4d6ce94980c5c2c9706ff
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-06 09:20:19 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.30 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,5 +1,5 @@
 def solution(price, money, count):
-    # 가격이 price인 놀이기구를 count번 타면 총 금액을 answer
-    answer = sum([price*i for i in range(1, count+1)])
-    # answer이 money보다 크면 answer-money, 작으면 0 리턴
-    return answer - money if answer > money else 0
+    result = 0
+    for i in range(1, count+1):
+        result += price * i
+    return result - money if result >= money else 0
