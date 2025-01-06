30634a7de9e6244ffee46f8ec37f0d58270b116d
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-06 09:11:15 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,10 +1,2 @@
 def solution(n):
-    # n을 절반
-    count = n // 2
-    
-    # n이 짝수라면
-    if n % 2 == 0:
-        return "수박" * count
-    # 홀수라면
-    else:
-        return "수박" * count + "수"
+    return ''.join(["수" if i % 2 == 0 else "박" for i in range(n)])
