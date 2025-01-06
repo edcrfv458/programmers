9095336f669792a4a2ef89a374ac812c619ad5ca
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-06 09:25:14 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,6 +1,7 @@
 def solution(s):
-    # s의 길이가 4또는 6이고 다 숫자로 되어있으면
-    if (len(s) == 4 or len(s) == 6) and s.isdigit():
-        return True     # True 리턴
-    else:   # 아니라면
-        return False    # False 리턴
+    if len(s) != 4 and len(s) != 6:
+        return False
+    elif s.isdigit() == False:
+        return False
+    else:
+        return True
