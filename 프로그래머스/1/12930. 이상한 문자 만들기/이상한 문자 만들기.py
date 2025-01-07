5830769dbbfb4eda9cdf601257ce36fed263a1d1
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-07 09:35:26 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,15 +1,13 @@
 def solution(s):
-    answer = ''     # 결과를 저장할 변수
-    idx = 0     # 각 단어에서 문자 위치 저장할 변수
-    for i in range(len(s)):
-        if s[i] == ' ':   # 공백이라면 한 단어가 끝난 것이므로
-            idx = 0     # 위치를 0으로 초기화
-            answer += s[i]  # 공백 문자 추가
-        else:
-            if idx % 2 == 0:    # 짝수는
-                answer += s[i].upper()  # 대문자로 저장   
-                idx += 1    # 위치 1증가
-            elif idx % 2 == 1:  # 홀수는
-                answer += s[i].lower()  # 소문자로 저장
-                idx += 1    # 위치 1증가
-    return answer
+    data = s.split(' ')
+    result = []
+    
+    for word in data:
+        answer = ""
+        for i in range(len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
+            else:
+                answer += word[i].lower()
+        result.append(answer)
+    return ' '.join(result)
