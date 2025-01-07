12917f06df589faefca665df1b9073d98c4b114c
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-07 09:09:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,10 +1,11 @@
 def solution(n, m):
-    # n의 약수
-    a = [i for i in range(1, n+1) if n % i == 0]
-    # m의 약수
-    b = [i for i in range(1, m+1) if m % i == 0]
-    # 최대 공약수
-    A = [i for i in a if i in b][-1]
-    # 최소 공배수
-    B = n*m // A
-    return [A,B]
+    a = max(n, m)
+    b = min(n, m)
+    
+    while (b > 0):
+        a, b = b, a % b
+    
+    gcd = a
+    lcm = n * m / gcd
+    
+    return [gcd, lcm]
