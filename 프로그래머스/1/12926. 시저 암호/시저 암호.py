6567834e160e90c5f50492a622656f65f25946fd
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-08 09:28:39 +0900

    [level 1] Title: 시저 암호, Time: 3.74 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,18 +1,15 @@
 def solution(s, n):
     answer = ""
-    for a in s:
-        i = 0
-        while i < n:
-            if a == 'z':
-                a = 'a'
-                i += 1
-            elif a == 'Z':
-                a = 'A'
-                i += 1
-            elif a == ' ':
-                break
+    for i in s:
+        if i == " ":
+            answer += " "
+            continue
+        for _ in range(n):
+            if i == 'z':
+                i = 'a'
+            elif i == 'Z':
+                i = 'A'
             else:
-                a = chr(ord(a) + 1)
-                i += 1
-        answer += a
+                i = chr(ord(i) + 1)
+        answer += i
     return answer
