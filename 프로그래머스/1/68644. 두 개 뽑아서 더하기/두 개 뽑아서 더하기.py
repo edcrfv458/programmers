b893c36aa1e69fae9d8da20faa6dd0bf72b29549
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-08 09:50:27 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.91 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,12 +1,8 @@
 def solution(numbers):
-    data = []   # 서로 다른 두 수를 더해서 저장할 리스트
-    # 0부터 len(numbers)-2 까지
-    for i in range(len(numbers)-1):
-        # i부터 len(numbers)-1 까지
+    data = []
+    for i in range(len(numbers) - 1):
         for j in range(i+1, len(numbers)):
-            # numbers에 i 요소와 j 요소를 더함
-            a = numbers[i] + numbers[j]
-            # data 리스트 안에 없다면 추가
-            if a not in data:
-                data.append(a)
-    return sorted(data)     # 정렬해서 리턴
+            s = numbers[i] + numbers[j]
+            if s not in data:
+                data.append(s)
+    return sorted(data)
