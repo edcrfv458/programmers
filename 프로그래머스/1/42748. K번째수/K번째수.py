d9349bb234459ffce7789e1d1f76e55c6196018d
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-08 09:43:47 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
-    result = []     # 결과를 저장할 배열
-    # commands 배열에 각 요소를 접근
-    for s, e, i in commands:
-        # s에서 e까지 슬라이싱하고 정렬
-        data = sorted(array[s-1:e])
-        # i번째 요소를 배열에 추가
-        result.append(data[i-1])
-    return result
+    answer = []
+    for s, e, n in commands:
+        data = array[s-1:e]
+        data.sort()
+        answer.append(data[n-1])
+    return answer
