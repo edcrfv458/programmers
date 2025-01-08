2e6d86671b56441086011aaf6c71920bd1f27a4e
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-08 09:16:01 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,8 +1,6 @@
 def solution(t, p):
-    count = 0   # p보다 작거나 같은 부분 문자열 수를 저장할 변수
-    # 0부터 len(t) - len(p) 까지 접근
+    count = 0
     for i in range(len(t) - len(p) + 1):
-        # 문자열 슬라이싱 한것이 p보다 작으면
         if t[i:i+len(p)] <= p:
-            count += 1  # count 1 증가
+            count += 1
     return count
