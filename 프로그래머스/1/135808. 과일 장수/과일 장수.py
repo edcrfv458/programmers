0deda920a3d295f3ea5496051cf4a8c6ce86d3a8
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-09 13:54:20 +0900

    [level 1] Title: 과일 장수, Time: 154.71 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,9 @@
 def solution(k, m, score):
+    result = 0
     score.sort(reverse=True)
-    answer = 0
-    j = len(score) % m
-    for i in range(0, len(score) - j, m):
-        s = min(score[i:i+m])
-        answer += m*s
-    return answer
+    for i in range(0, len(score), m):
+        data = score[i:min(len(score), i+m)]
+        if len(data) == m:
+            result += data[-1] * m
+    return result
+        
