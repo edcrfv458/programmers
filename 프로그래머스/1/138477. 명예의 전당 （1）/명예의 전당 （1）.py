d2b44ae20cf2f317fbc57f4db250aae25076750c
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-09 09:45:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,8 +1,12 @@
 def solution(k, score):
-    answer = []     # 각 날짜 별로 최저 점수를 저장할 리스트
-    data = []       # 각 일차의 점수를 저장
-    for i in score:
-        data.append(i)      # data 리스트에 추가하고
-        # 각 일차의 data를 정렬하고 k번째까지 자르고 마지막 요소 추가
-        answer.append(sorted(data, reverse=True)[:k][-1])
-    return answer
+    result = [] # 최하위 점수 저장
+    data = []   # 점수 저장
+    for s in score:
+        if len(data) < k:
+            data.append(s)
+        else:
+            if s > data[-1]:
+                data[-1] = s
+        data.sort(reverse=True)
+        result.append(data[-1])
+    return result
