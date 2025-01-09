0bb6c5c064abfadd18597701efb94e024f53b61b
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-09 09:26:56 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,5 +1,5 @@
 def solution(food):
     answer = ""
-    for i, d in enumerate(food):
-        answer += str(i) * (d // 2)
+    for i, f in enumerate(food):
+        answer += str(i) * (f // 2)
     return answer + "0" + answer[::-1]
