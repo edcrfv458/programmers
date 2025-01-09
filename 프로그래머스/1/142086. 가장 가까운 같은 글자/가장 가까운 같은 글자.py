ca1ee59a06c58c26f5d076b5b291660ef64b7a19
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-09 09:13:33 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1645.30 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,22 +1,13 @@
 def solution(s):
-    # 등장한 단어와 인덱스를 저장할 딕셔너리 생성
-    save = {}
-    
-    # 결과를 저장할 리스트 생성
     result = []
-    
-    for i, w in enumerate(s):
-        # 단어가 사전의 키 값들 중 존재하지 않으면
-        if w not in save.keys():
-            # 사전에 단어와 인덱스 저장하고
-            save[w] = i
-            # 리스트에 -1 추가
+    number = []
+    for i, c in enumerate(s):
+        if c not in number:
             result.append(-1)
-        # 단어가 사전의 키 값들 중 존재하면
+            number.append(c)
         else:
-            # 리스트에 현재 인덱스에서 사전에 존재하는 단어의 인덱스를 뺀 값을 저장하고
-            result.append(i - save.get(w))
-            # 사전에 단어의 인덱스를 업데이트
-            save[w] = i
-            
+            a = number.index(c)
+            result.append(i - a)
+            number[a] = 0
+            number.append(c)
     return result
