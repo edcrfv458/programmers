864603a44e4c1afe8fa64489056c27c06d70c21f
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-13 09:59:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 4923.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,14 +1,14 @@
 def solution(number, limit, power):
     result = 0
     for i in range(1, number+1):
-        div = 0  # 약수의 개수
+        count = 0
         for j in range(1, int(i**0.5)+1):
             if j**2 == i:
-                div += 1
-            elif i % j == 0:
-                div += 2
-        if div <= limit:
-            result += div
+                count += 1
+            elif i%j == 0:
+                count += 2
+        if count <= limit:
+            result += count
         else:
             result += power
     return result
