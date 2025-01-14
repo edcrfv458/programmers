75ceec1cd7671b3b1cb83b6f35e7044baaeed44b
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-14 09:26:24 +0900

    [level 1] Title: 옹알이 (2), Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,14 +1,14 @@
 def solution(babbling):
-    count = 0
+    result = 0
     for b in babbling:
-        if 'aya' * 2 not in b:
-            b = b.replace('aya', ' ')
-        if 'ye' * 2 not in b:
-            b = b.replace('ye', ' ')
-        if 'woo' * 2 not in b:
-            b = b.replace('woo', ' ')
-        if 'ma' * 2 not in b:
-            b = b.replace('ma', ' ')
-        if not b.replace(' ', ''):
-            count += 1
-    return count
+        if 'aya'*2 not in b:
+            b = b.replace("aya", " ")
+        if 'ye'*2 not in b:
+            b = b.replace("ye", " ")
+        if 'woo'*2 not in b:
+            b = b.replace("woo", " ")
+        if 'ma'*2 not in b:
+            b = b.replace("ma", " ")
+        if not b.replace(" ", ''):
+            result += 1
+    return result
