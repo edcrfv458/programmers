cab425744127131bd2e2347a27834e4219a77648
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-14 09:41:25 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1790.95 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,20 +1,22 @@
 def solution(X, Y):
-    x = []
-    y = []
     result = []
+    
+    x, y = [], []
     for i in X:
         x.append(i)
     for i in Y:
         y.append(i)
-    x_set = set(x)
-    y_set = set(y)
-    xy = x_set & y_set
-    if not xy:
+        
+    set_x, set_y = set(x), set(y)
+    set_xy = set_x & set_y
+
+    if not set_xy:
         return "-1"
-    if len(xy) == 1 and "0" in xy:
+    elif len(set_xy) == 1 and "0" in set_xy:
         return "0"
-    for i in xy:
-        c = min(x.count(i), y.count(i))
-        for _ in range(c):
-            result.append(int(i))
-    return ''.join(sorted(map(str, result), reverse=True))
+    
+    for i in set_xy:
+        for _ in range(min(x.count(i), y.count(i))):
+            result.append(i)
+    
+    return ''.join(sorted(result, reverse=True))
