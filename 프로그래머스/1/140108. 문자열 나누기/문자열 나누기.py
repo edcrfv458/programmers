9e6d044697386783f38b7c99e590a861bcd772ab
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-17 09:45:39 +0900

    [level 1] Title: 문자열 나누기, Time: 1.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,22 +1,20 @@
 def solution(s):
-    count = 0           # 문자열 분리 수
-    i = 0               # 확인할 요소의 인덱스
-    i_count = 0         # 첫번째 문자와 같은 문자의 수
-    not_i_count = 0     # 첫번째 문자와 다른 문자의 수  
-    start = s[i]        # 첫번째 문자
-    while i < len(s):
-        if s[i] == start:
-            i_count += 1
+    count, x_count, o_count = 0, 0, 0
+    word = s[0]
+    
+    for i, c in enumerate(s):
+        if c == word:
+            o_count += 1
         else:
-            not_i_count += 1
-        i += 1
-        if i_count == not_i_count:
+            x_count += 1
+        
+        if o_count == x_count:
             count += 1
-            if i >= len(s):
-                return count
-            start = s[i]
-            i_count = 0
-            not_i_count = 0
-    if i_count != not_i_count:
-        return count+1
+            x_count, o_count = 0, 0
+            if i+1 < len(s):
+                word = s[i+1]
+    if o_count > 0 or x_count > 1:
+        count += 1
+    
+    return count
             
