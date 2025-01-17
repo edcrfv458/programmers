9f1be7b365fa104e88682964e060936a718ace87
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-17 09:57:11 +0900

    [level 1] Title: 대충 만든 자판, Time: 38.96 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,16 +1,16 @@
 def solution(keymap, targets):
-    answer = []
+    result = []
     for target in targets:
         count = 0
         for t in target:
             a = 101
             for key in keymap:
                 if t in key:
-                    a = min(a, key.index(t)+1)
+                    a = min(a, key.index(t) + 1)
             if a != 101:
                 count += a
-            elif a == 101:
+            else:
                 count = -1
                 break
-        answer.append(count)
-    return answer
+        result.append(count)
+    return result
