33eac2045a3de4e5417acb6cf6ea4c711a4f8b2f
edcrfv458 <49359874+edcrfv458@users.noreply.github.com>
2025-01-20 09:23:21 +0900

    [level 1] Title: 둘만의 암호, Time: 0.42 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,15 +1,13 @@
 def solution(s, skip, index):
-    answer = ""
-    skip_set = set(skip)  # 빠른 검색을 위해 set으로 변환
+    skip_set = set(skip)
+    result = ""
     
-    for i in s:
-        count = 0
-        while count < index:
-            i = chr(ord(i) + 1)  # 현재 문자에서 1만큼 이동
-            if i > 'z':  # z를 넘어서면 a로 순환
-                i = 'a'
-            if i not in skip_set:  # skip에 없는 경우에만 count 증가
-                count += 1
-        answer += i
+    for char in s:
+        step = 0
+        while step < index:
+            char = 'a' if char == 'z' else chr(ord(char) + 1)
+            if char not in skip_set:
+                step += 1
+        result += char
     
-    return answer
+    return result
